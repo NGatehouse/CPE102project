5882122f2a1788e860d002891b624a9f01f6fb2b
lrfox <lhickman@calpoly.edu>
2015-04-13 16:28:27 -0700

    Color-coded design2

---
+++ DESIGN2.py
@@ -0,0 +1,16 @@
+Entities.py
+   
+   Objects
+      Background
+      Obstacle
+      Time_management #scheduling class
+      Actors #possibly next_possition and blob_next_possition from worldmodel.py
+         Blacksmith
+         Miner
+            MinerNotFull
+            MinerFull
+         Ore
+            OreBlob
+         Vein
+         Quake
+         

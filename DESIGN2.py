52bd07a69107d94eb0d312e849f27a7b52d5f96e
lrfox <lhickman@calpoly.edu>
2015-04-17 15:23:14 -0700

    Update for Friday on Des2

--- DESIGN2.py
+++ DESIGN2.py
@@ -2,21 +2,32 @@ Entities.py
    Entity #get_images get_image next_image get_name 
       Background(Entity)       
       On_Grid(Entity) # set_position , get_position
-        Obstacle(On_Grid)   # add entity_string  ?????????????????
-        Actor(On_Grid, Action_manager) 
-         # get_rate add_pending_actions remove_pending_actions clear_pending_actions get_pending_actions 
-            Ore(Actor) # create_ore_transform_action add entity_string  ?????????????????           
-            Vein(Actor) # create_vein_action add entity_string  ?????????????????        
-            Motion_actor(Actor,Animation_manager)  #possibly next_possition and blob_next_possition from worldmodel.py, create_entity_death_action, (maybe self_to_other) (maybe create_actor_action)
-                OreBlob(Motion_actor) #blob_to_vein create_ore_blob_action
-                Quake(Motion_actor) #redefine get_rate as pass ????????????
+        Obstacle(On_Grid)   #    
+        Actor(On_Grid,action_manger) 
+         # get_rate add_pending_actions remove_pending_actions clear_pending_actions get_pending_actions  
+            Ore(Actor) # create_ore_transform_action              
+            Vein(Actor) # create_vein_action           
+            Motion_actor(Actor)  #possibly next_possition and blob_next_possition from worldmodel.py, create_entity_death_action, (maybe self_to_other) (maybe create_actor_action)  (takes animation_rate)
+                OreBlob(Motion_actor,animation_manager) #blob_to_vein create_ore_blob_action
+                Quake(Motion_actor) 
             Mining(Actor) #3 actors:  set_resource_count get_resource_count set_resource_limit               
                  Blacksmith(Mining) 
-                 Miner(Mining,Motion_actor) #create_miner_action try_transform_miner, add entity string ??
+                 Miner(Mining,Motion_actor, animation_manager) #create_miner_action try_transform_miner, animation_rate
                      MinerNotFull(Miner) #try_transform_miner_not_full miner_to_ore create_miner_not_full_action
                      MinerFull(Miner) #try_transform_miner_full miner_to_smith
    Action_manager #scheduling class, schedule_action, 
-      Animation_manager(Action_manager) # schedule_animation create_animation_action   get_animation_rate                
+   Animation_manager # schedule_animation create_animation_action   get_animation_rate 
+      
+
+#What is the difference between    super(b,self).__init__(x)  and  A.__init__(self,x) 
+   # We used the latter in MinerNotFull and MinerFull because super() raises “TypeError: must be type, not classobj”
+     
+#In Blacksmith resource_count is not in __init__. How does this work?      
+
+
+
+
+      
 # Question 1... maybe self_to_other method for motion actor instead of having separate methods.. i.e. (blob_to_vein,miner_to_ore,miner_to_smith)     
 Yes  
 # Question 2... add entity_string to 4 different classes.. Obstacle, Ore, Vein, Miner[full & notfull same thing?? since they are a "miner"] 

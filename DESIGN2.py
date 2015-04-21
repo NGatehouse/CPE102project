c6968756e111170a6187504529f3a142124f843e
lrfox <lhickman@calpoly.edu>
2015-04-20 17:15:31 -0700

    ALMOST THERE!

--- DESIGN2.py
+++ DESIGN2.py
@@ -1,24 +1,45 @@
 Entities.py   
-   Entity #get_images  get_image  next_image  get_name  DONE
-      Background(Entity) # DONE       
-      On_Grid(Entity) # set_position  get_position DONE
-        Quake(On_Grid, animation_manager, action_manger) 
-        Obstacle(On_Grid) #DONE   
-        Actor(On_Grid,action_manger) 
-         # get_rate 
-            Ore(Actor) # create_ore_transform_action   DONE            
-            Vein(Actor) # create_vein_action DONE            
-            OreBlob(Actor,animation_manager) #create_ore_blob_action
-            Mining(Actor) '''Interface'''#set_resource_count get_resource_count  set_resource_limit   DONE
-                 Blacksmith(Mining) DONE
-                 Miner(Mining,Motion_actor, animation_manager) #create_miner_action  try_transform_miner animation_rate   
-                     MinerNotFull(Miner) #try_transform_miner_not_full   create_miner_not_full_action DONE
-                     MinerFull(Miner) #try_transform_miner_full   DONE
-   Action_manager '''Interface'''#scheduling class, schedule_action, 
-                  #add_pending_actions remove_pending_actions clear_pending_actions get_pending_actions  , create_entity_death_action
-   Animation_manager '''Interface'''# schedule_animation create_animation_action   get_animation_rate  
+   Entity #DATA name images
+   #get_images  get_image  next_image  get_name 
+      Background(Entity)        
+      On_Grid(Entity) #DATA name images position
+        #set_position  get_position 
+        Quake(On_Grid, animation_manager, action_manger) #DATA name  position  images  animation_rate
+        Obstacle(On_Grid) #name  position  images
+        #entity_string
+        Actor(On_Grid,action_manger) #DATA name position rate images
+         #get_rate 
+            Ore(Actor) #DATA name  position  images  rate (=5000)
+            #entity_string  create_ore_transform_action
+            Vein(Actor) #DATA name  rate  position  images  resource_distance (=1) 
+            #create_actor_motion  entity_string  get_resource_distance             
+            OreBlob(Actor,animation_manager) #DATA name  position  rate images  animation_rate
+            #_to_other  create_actor_motion
+            Mining(Actor) '''Interface'''#DATA name  resoure_limit  position rate  images
+            #set_resource_count get_resource_count  set_resource_limit   
+                 Blacksmith(Mining) #DATA name  position  images  resoure_limit  rate  resource_distance(defaulted to 1)
+                 #get_resource_distance  entity_string
+                 Miner(Mining, animation_manager) #DATA name resoure_limit  position  rate  images animation_rate
+                 #create_miner_action  try_transform_miner entity_string   
+                     MinerNotFull(Miner) #DATA name resoure_limit  position  rate  images animation_rate
+                     #try_transform  to_other   create_actor_motion 
+                     MinerFull(Miner) #DATA name resoure_limit  position  rate  images animation_rate
+                     #try_transform  to_other   create_actor_motion 
+   Action_manager '''Interface''' #remove_pending_action  get_pending_action  clear_pending_actions  schedule_action  create_entity_death_action
+   Animation_manager '''Interface''' #get_animation_rate  create_animation_action  schedule_animation
       
-_____________________________________________________________________     
+_____________________________________________________________________   
+
+                             QUESTIONS
+
+entity_string #should be
+
+#the last functions in actions.py (create and schedule)
+
+get_resource_distance
+
+transform_action
+_____________________________________________________________________  
 
 #What is the difference between    super(b,self).__init__(x)  and  A.__init__(self,x) 
    # We used the latter in MinerNotFull and MinerFull because super() raises “TypeError: must be type, not classobj”

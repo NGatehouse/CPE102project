84e3e1013192c925dbcddb2d117585af8957a02f
lrfox <lhickman@calpoly.edu>
2015-04-27 20:08:40 -0700

    Project and tests setup

---
+++ !run main.py.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Luke\cpe102\experiment\CPE102project_o\
+C:\Python27\python main.py
+PAUSE
---
+++ java/tests/tests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class tests {
+    private static final double DELTA = 0.00001;
+
+    @Test
+    public void test_1() {
+        assertEquals(1.0, 1.0, DELTA);
+    }
+
+    @Test
+    public void test_2() {
+        assertEquals(1.0, 1.00000000000001, DELTA);
+    }
+
+}

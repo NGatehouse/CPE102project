5a449597692e65612f54a244a68ca73634a21c4d
lrfox <lhickman@calpoly.edu>
2015-04-27 17:25:57 -0700

    IntelliJ project created

---
+++ java/src/worldmodel.java
@@ -0,0 +1,25 @@
+public class worldmodel {
+
+    // fields
+    //Do we want to make fields public or private?
+    private int num_rows;
+    private int num_cols;
+    private int background;
+    private int occupancy;
+    private int entities;
+
+
+    // constructor
+    public WorldModel(int num_rows, int num_cols, int background) {
+        this.background = occ_grid.Grid(num_cols, num_rows, background);
+        this.num_rows = num_rows;
+        this.num_cols = num_cols;
+        this.occupancy = occ_grid.Grid(num_cols, num_rows, None);
+        this.entities = [];
+        //this.action_queue = ordered_list.OrderedList();
+    }
+
+    // methods
+
+
+}

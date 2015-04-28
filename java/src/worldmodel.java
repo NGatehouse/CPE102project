cddf126da6b0faa7269a696072897af8242e7ea5
rottren <ngatehou@calpoly.edu>
2015-04-28 15:43:36 -0700

    worldmodel

--- java/src/worldmodel.java
+++ java/src/worldmodel.java
@@ -14,8 +14,9 @@ public class WorldModel {
         this.background = occ_grid.Grid(num_cols, num_rows, background);
         this.num_rows = num_rows;
         this.num_cols = num_cols;
-        this.occupancy = occ_grid.Grid(num_cols, num_rows, None);
+        this.occupancy = occ_grid.Grid(num_cols, num_rows, null); // null was None fyi
         this.entities = [];
+
         //this.action_queue = ordered_list.OrderedList();
     }
 

fb3d8533399e9a05b5a2b74b52f73136668e0c04
rottren <ngatehou@calpoly.edu>
2015-04-28 14:17:45 -0700

    hh

--- java/src/Miner.java
+++ java/src/Miner.java
@@ -1,15 +1,15 @@
 public class Miner extends Mining
     //implements Animation_manager missing data imgs,animation_rate
 {
-
+    private Point position;
     public Miner(String name,int resource_limit,Point position,int rate)
     {
         super(name,resource_limit,position,rate);
     }
 
-   /* public String entity_string()
+    public String entity_string()
     {
-        return;
-    }*/
+        return 'miner' + this.get_name() + Integer.toString(this.position.get_x()) + Integer.toString(this.position.get_y()) + Integer.toString(this.resource_limit) + Integer.toString(this.rate);
+    }
 
 }
--- java/src/Mining.java
+++ java/src/Mining.java
@@ -1,20 +1,26 @@
 public class Mining extends Actor
 {
+    final int RESOURCE_COUNT = 0;
     private int resource_limit;
-    private Point position;
-    public Mining(String name, int resource_limit,Point position, int rate)
+    private int resource_count;
+    public Mining(String name, int resource_limit,Point position, int rate,int resource_count)
     {
         super(name,position,rate);
         this.resource_limit=resource_limit;
-        //what do you do for constants?
+        this.resource_count=RESOURCE_COUNT;
     }
 
     public int get_resource_limit()
     {
         return this.resource_limit;
     }
-    /*public int get_resource_count()
+
+    public int get_resource_count()
     {
         return this.resource_count;
-    }*/
+    }
+    public void set_resource_count(int n)
+    {
+        this.resource_count=n;
+    }
 }

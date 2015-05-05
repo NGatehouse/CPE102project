6899da9e91146f750ca7c6cae99659ab2afa4b22
lrfox <lhickman@calpoly.edu>
2015-05-04 22:40:28 -0700

    adsf

--- java/tests/WorldModelTests.java
+++ java/tests/WorldModelTests.java
@@ -18,8 +18,6 @@ public class WorldModelTests
         assertEquals(1.0, 1.00000000000001, DELTA);
     }
 
-
-
     @Test
     public void test_find_open_around()
     {
@@ -32,18 +30,50 @@ public class WorldModelTests
     @Test
     public void test_find_open_around_ifnot()
     {
-        Point pt = new Point(3,4);
+        Point pt = new Point(20,50);
         int distance = 5;
         WorldModel world = new WorldModel(5,5);
-        assertTrue(pt.equals(world.find_open_around(pt, distance)));
+        assertTrue(!pt.equals(world.find_open_around(pt, distance)));
     }
 
     @Test
     public void test_blob_next_pos()
     {
-        Point pt = new Point(3,4);
-        int distance = 5;
+        Point entpt = new Point(1,1);
+        Point destpt = new Point(2,2);
+        Point newpt = new Point(0,-1);
         WorldModel world = new WorldModel(5,5);
-        assertTrue(pt.equals(world.find_open_around(pt, distance)));
+        assertTrue(newpt.get_x() == world.blob_next_position(entpt, destpt).get_x());
     }
+
+    @Test
+    public void test_next_position()
+    {
+        Point entpt = new Point(1,1);
+        Point destpt = new Point(2,2);
+        Point newpt = new Point(0,-1);
+        WorldModel world = new WorldModel(5,5);
+        assertTrue(newpt.get_x() == world.next_position(entpt, destpt).get_x());
+    }
+
+    @Test
+    public void test_within_boundsT()
+    {
+        Point pt = new Point(1,1);
+        WorldModel world = new WorldModel(5,5);
+        assertTrue(world.within_bounds(pt));
+    }
+
+    @Test
+    public void test_within_boundsF()
+    {
+        Point pt = new Point(50,555);
+        WorldModel world = new WorldModel(5,5);
+        assertTrue(world.within_bounds(pt));
+    }
+
+
+
+
+
 }

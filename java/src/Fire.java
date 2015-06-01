11ff400870dfa8347c8a61bb32298848dd93fbd0
rottren <ngatehou@calpoly.edu>
2015-06-01 13:35:58 -0700

    added wood && fire

---
+++ java/src/Fire.java
@@ -0,0 +1,11 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Fire extends On_Grid
+{
+    public Fire(String name,Point position,List<PImage> imgs)
+    {
+        super(name,position,imgs);
+    }
+}
---
+++ java/src/Wood.java
@@ -0,0 +1,11 @@
+import processing.core.PImage;
+
+import java.util.List;
+
+public class Wood extends On_Grid
+{
+    public Wood(String name,Point position,List<PImage> imgs)
+    {
+        super(name,position,imgs);
+    }
+}
